7954f580fffb6f7dd6791ae1226b833a11d7ee52
Mogamat-Muneeb <davimuneeb785@gmail.com>
2022-02-08 11:47:55 +0200

    done

--- routes/testimonialRoutes.js
+++ routes/testimonialRoutes.js
@@ -8,45 +8,45 @@ let testimonials = [
      "img":"https://i.postimg.cc/FRkTVGGL/Breda-Y1-6-11zon.jpg",
      "email":"mugammadbreda1999@gmail.com",
      "quote":"This man is hardworking as well as efficient with his time.His a great guy to work with.",
-     "relation":"Colleague"
-},
-{"title":" Yaseen Davids",
- "id":2,
- "img":"https://i.postimg.cc/k5Bs7w0p/Yaseen3-9-11zon.jpg",
- "email":"yaaseendav@gmail.com",
- "quote":"Muneeb is a great designer and is always willing to help the next person.",
- "relation":"Colleague"
-},
+        "relation":"Colleague"
+    },
+    {"title":" Yaseen Davids",
+    "id":2,
+    "img":"https://i.postimg.cc/k5Bs7w0p/Yaseen3-9-11zon.jpg",
+    "email":"yaaseendav@gmail.com",
+    "quote":"Muneeb is a great designer and is always willing to help the next person.",
+    "relation":"Colleague"
+    },
 
-{"title":" Ndomizanele Moni",
- "id":3,
- "img":"https://i.postimg.cc/TwbcD1Yj/Zanele3-10-11zon.jpg",
- "email":"zanelemoni4@gmail.com",
- "quote":"Muneeb is a good person,he is always willing to help. He is always focused on the task at hand and is not easily distracted,HE LOVES HIS WORK.",
- "relation":"Colleague"
-},
+    {"title":" Ndomizanele Moni",
+    "id":3,
+    "img":"https://i.postimg.cc/TwbcD1Yj/Zanele3-10-11zon.jpg",
+    "email":"zanelemoni4@gmail.com",
+    "quote":"Muneeb is a good person,he is always willing to help. He is always focused on the task at hand and is not easily distracted,HE LOVES HIS WORK.",
+    "relation":"Colleague"
+    },
 
-{"title":"  Na-aim Fredericks",
- "id":4,
- "img":"https://i.postimg.cc/8zkTLjVH/Na-aim3-7-11zon.jpg",
- "email":"fredericksnaam13@gmail.com",
- "quote":"Muneeb is a very accountable guy when it comes to being a team player he is always flexible in his work he is ethical and loyal.",
- "relation":"Colleague"
-},
+    {"title":"  Na-aim Fredericks",
+    "id":4,
+    "img":"https://i.postimg.cc/8zkTLjVH/Na-aim3-7-11zon.jpg",
+    "email":"fredericksnaam13@gmail.com",
+    "quote":"Muneeb is a very accountable guy when it comes to being a team player he is always flexible in his work he is ethical and loyal.",
+    "relation":"Colleague"
+    },
 
-{"title":" Simamkele January",
- "id":5,
- "img":"https://i.postimg.cc/dv7gPntj/Simamkele2-8-11zon.jpg",
- "email":"simamkelejanjuary@gamil.com",
- "quote":"Besides bieng an outstanding classmate, Muneeb is a free spirited young man who is always ready to help out where he is needed.",
- "relation":"Colleague"
-},
-{"title":"  Alex Sexwale",
- "id":6,
- "img":"https://i.postimg.cc/vB4NSG8D/alex-5-11zon.jpg",
- "email":"alexsexwale@gmail.com",
- "quote":"Muneeeb is vigilant with his work. He continues to find ways to provide quality work and keep improving on his general knowledge.","relation":"Lecturer"
-}
+    {"title":" Simamkele January",
+    "id":5,
+    "img":"https://i.postimg.cc/dv7gPntj/Simamkele2-8-11zon.jpg",
+    "email":"simamkelejanjuary@gamil.com",
+    "quote":"Besides bieng an outstanding classmate, Muneeb is a free spirited young man who is always ready to help out where he is needed.",
+    "relation":"Colleague"
+    },
+    {"title":"  Alex Sexwale",
+    "id":6,
+    "img":"https://i.postimg.cc/vB4NSG8D/alex-5-11zon.jpg",
+    "email":"alexsexwale@gmail.com",
+    "quote":"Muneeeb is vigilant with his work. He continues to find ways to provide quality work and keep improving on his general knowledge.","relation":"Lecturer"
+    }
 
     ];
         
