6fd82aba6261202bff16f188b7cbea7c762e056d
Mogamat-Muneeb <davimuneeb785@gmail.com>
2022-10-27 20:25:04 +0200

    make it better projects edited2

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -7,7 +7,7 @@ let projects = [
     // "id":1,
     // "img":"https://i.postimg.cc/N086frVf/Screenshot-41.png",
     // "github":"https://github.com/Mogamat-Muneeb/online-store",
-    // "netlify":"https://dinghystore.netlify.app",
+    // "link":"https://dinghystore.link.app",
     // "lang":"HTML/CSS",
     // "desc":"An online store that sells one product."
     // },
@@ -15,7 +15,7 @@ let projects = [
     "id":1,
     "img":"https://i.postimg.cc/FsW-qgkZd/Screenshot-49.png",
     "github":"https://github.com/Mogamat-Muneeb/calculator_2",
-    "netlify":"https://jscalculator2.netlify.app",
+    "link":"https://jscalculator2.link.app",
     "lang":"JS HTML CSS",
     "desc":"A basic calculator with full functionality."
     },
@@ -23,14 +23,14 @@ let projects = [
     // "id":4,
     // "img":"https://i.postimg.cc/rFf6nP8p/Screenshot-50.png",
     // "github":"https://github.com/Mogamat-Muneeb/reaction-time-vue-",
-    // "netlify":"https://muneebs-reacting-timer.netlify.app/",
+    // "link":"https://muneebs-reacting-timer.link.app/",
     // "lang":"VUE.JS",
     // "desc":"Mini game to test eye coordination. "
     // },
     {"title":"Davids Driving School",
     "id":2,"img":"https://i.postimg.cc/pdcR6BTr/Screenshot-42.png", 
     "github":"https://github.com/Mogamat-Muneeb/davids_drivingschool",
-    "netlify":"https://davids-drivng-school.netlify.app",
+    "link":"https://davids-drivng-school.link.app",
     "lang":"HTML CSS",
     "desc":"A brochure site that advertises a driving school."
     },
@@ -38,14 +38,14 @@ let projects = [
     //  "id":5,
     //  "img":"https://i.postimg.cc/mk9zSyr9/Screenshot-16.png",
     //  "github":"https://github.com/Mogamat-Muneeb/POS_Project",
-    //  "netlify":"https://neebspos.netlify.app/",
+    //  "link":"https://neebspos.link.app/",
     //  "lang":"JS",
     //  "desc":"A typical online store."
     // },
      {"title":"Temp-Convertor",
      "id":3,"img":"https://i.postimg.cc/fRVwYBJj/Screenshot-37.png",
      "github":"https://github.com/Mogamat-Muneeb/TEMPERATURE-CONVERTOR",
-     "netlify":"https://calcutemp.netlify.app/",
+     "link":"https://calcutemp.link.app/",
      "lang":"JS HTML CSS",
      "desc":"Converts from Celsius to Fahrenheit. "
     },
@@ -53,7 +53,7 @@ let projects = [
     "id":4,
     "img":"https://i.postimg.cc/sxt8Wz7r/Screenshot-1.png",
     "github":"https://github.com/Mogamat-Muneeb/BARBERSHOP-FRONTEND",
-    "netlify":"https://the-urbanshave-barbershop.web.app/",
+    "link":"https://the-urbanshave-barbershop.web.app/",
     "lang":"VUE.JS NODE.JS MONGODB",
     "desc":"A brochure site with booking section. "
     },
@@ -61,7 +61,7 @@ let projects = [
     // "id":4,
     // "img":"https://i.postimg.cc/j2N5338X/Screenshot-53.png",
     // "github":"https://github.com/Mogamat-Muneeb/surfstore-front",
-    // "netlify":"https://surfstore-proj.web.app/",
+    // "link":"https://surfstore-proj.web.app/",
     // "lang":"VUE.JS/NODE.JS/MONGODB",
     // "desc":"Simple Point Of Sale. "
     // },
@@ -69,7 +69,7 @@ let projects = [
     "id":5,
     "img":"https://i.postimg.cc/dt2Qkdw1/Screenshot-22.png",
     "github":"https://github.com/Mullins69/blogplatform_backend",
-    "netlify":"https://the-blog3.web.app/#/",
+    "link":"https://the-blog3.web.app/#/",
     "lang":"VUE.JS NODE.JS MONGODB BOOTSTRAP",
     "desc":"A Blog Platform. A Group Project. "
     },
@@ -98,15 +98,15 @@ app.get('/:id',(req,res) => {
 })
 //create a project
 app.post('/',(req,res) => {
-    let {title ,img ,github ,netlify, lang , desc} = req.body;
-    if (!title || !img || !github || !netlify || !lang || !desc)
+    let {title ,img ,github ,link, lang , desc} = req.body;
+    if (!title || !img || !github || !link || !lang || !desc)
      res.status(400).send({msg: 'not all info was sent'});
 let newProject = {
     id:projects.length + 1,
     title,
     img,
     github,
-    netlify,
+    link,
     lang,
     desc,
    
@@ -119,13 +119,13 @@ res.send(newProject);
 app.put('/:id',(req,res) =>{
     let project = projects.find((project) => project.id == req.params.id);
     if (!project) res.status(404).send({msg: 'projects not found'});
-    let (title,img, github,netlify,lang,desc) = req.body;
+    let (title,img, github,link,lang,desc) = req.body;
 
     
         if (title) project. title = title;
         if (img) project.img = img;
         if (github) project.github = github;
-        if (netlify) project.netlify = netlify;
+        if (link) project.link = link;
         if (lang) project.lang = lang;
         if (desc) project.desc = desc;
         res.send(project);  
